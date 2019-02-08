8f3651fae1767dec748e8f54be14214b17e22abe
SaraHisham <36595602+SaraHisham@users.noreply.github.com>
2019-02-08 22:05:36 +0200

    Create search.php

---
+++ backend/search.php
@@ -0,0 +1,145 @@
+<?php error_reporting(0); ?>
+<?php
+
+session_start();
+
+$search_bar ="";
+$period = "";
+$type = "";
+$field = "";
+$city= "";
+$academic_year = "";
+$errors = array();
+
+if (isset($_POST['searchbtn'])) {
+  $search_bar = $_POST['searchBar'];
+  $period = $_POST['period'];
+  $type = $_POST['type'];
+  $field = $_POST['field'];
+  $city= $_POST['city'];
+  $academic_year= $_POST['Academic_Year'];
+
+// Empty Search Bar
+
+if (empty($search_bar)){
+    array_push($errors, "Invalid Search");
+  }
+
+ // Select records from Internship table in Database
+// $query = mysql_select("internship", "AND" , array (
+//             "period" => $period,
+//             "title" => $search_bar,
+//             "type" => $type,
+//             "field" => $field,
+//             "city" => $city,
+//
+//
+//   )
+
+
+
+
+
+//   $result = mysqli_query($connect, $query);
+//
+//     // if id exist
+//     // show data in inputs
+//     if(mysqli_num_rows($result) > 0)
+//     {
+//       while ($row = mysqli_fetch_array($result))
+//       {
+//         $fname = $row['fname'];
+//         $lname = $row['lname'];
+//         $age = $row['age'];
+//       }
+//     }
+//
+//     // if the id not exist
+//     // show a message and clear inputs
+//     else {
+//         echo "Undifined ID";
+//             $fname = "";
+//             $lname = "";
+//             $age = "";
+//     }
+//
+//
+//     mysqli_free_result($result);
+//     mysqli_close($connect);
+//
+// }
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Search Results</title>
+  </head>
+  <style>
+  table {
+   border-collapse: collapse;
+   width: 100%;
+   color: #588c7e;
+   font-family: monospace;
+   font-size: 25px;
+   text-align: left;
+     }
+  th {
+   background-color: #588c7e;
+   color: white;
+    }
+  tr:nth-child(even) {background-color: #f2f2f2}
+ </style>
+  <body>
+    <body>
+     <table>
+     <tr>
+      <th>Title</th>
+      <th>Company</th>
+      <th>Field</th>
+      <th>City</th>
+      <th>Period</th>
+      <th>Academic Year</th>
+      <th>Type</th>
+      <th>Role</th>
+      <th>Description</th>
+      <th>Deadline</th>
+     </tr>
+     <?php
+    $conn = mysqli_connect("localhost", "root", "", "internship");
+      // Check connection
+      if ($conn->connect_error) {
+       die("Connection failed: " . $conn->connect_error);
+      }
+      // $sql = "mysql_select("internship", "AND" , array (
+      //             "period" => $period,
+      //             "title" => $search_bar,
+      //             "type" => $type,
+      //             "field" => $field,
+      //             "city" => $city,
+      $sql="SELECT * from internship where period = $period And type = $type And field = $field And city = $city And academic_year = $academic_year";
+      //           $statement=$db->prepare($sql);
+      //           $statement->execute([$period , $type, $field, $city, $academic_year]);
+          //  $sql = "SELECT from internship where "
+
+      $result = $conn->query($sql);
+      if ($result->num_rows > 0) {
+       // output data of each row
+       while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["title"]. "</td><td>" . $row["company"]. "</td><td>" . $row["field"]. "</td></tr>" . $row["city"]. "</td><td>" . $row["period"]. "</td><td>" . $row["academic_year"] . "</td><td>"
+        . $row["type"] . "</td><td>"
+. $row["role"] . "</td><td>" . $row["description"] . "</td><td>" . $row["deadline"] . "</td><td>";
+    }
+    echo "</table>";
+    } else { echo "0 results"; }
+    $conn->close();
+    ?>
+    </table>
+    </body>
+    </html>
+
+  </body>
+</html>
