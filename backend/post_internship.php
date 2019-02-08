a7c4935e9cf17d909828389d2d50d4d5209f00c7
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-08 20:46:01 +0200

    Switch to database constants

--- backend/post_internship.php
+++ backend/post_internship.php
@@ -5,67 +5,11 @@ function getName($name)
 {
 	return str_replace("_", " ", $name);
 }
-$fields = array("Mechanical Power Engineering",
-"Computer Engineering",
-"Electrical Power Engineering",
-"Communication Systems Engineering",
-"Civil Engineering",
-"Architectural Engineering",
-"Marketing",
-"Sales",
-"Accounting",
-"Advertisement",
-"Pharmaceutical",
-"Cardiology",
-"Dermatology",
-"Immunology",
-"Neurology",
-"Sports",
-"Miscellaneous");
-$cities = array("Cairo",
-"Alexandria",
-"Gizeh",
-"Shubra El-Kheima",
-"Port Said",
-"Suez",
-"Luxor",
-"al-Mansura",
-"El-Mahalla El-Kubra",
-"Tanta",
-"Asyut",
-"Ismailia",
-"Fayyum",
-"Zagazig",
-"Aswan",
-"Damietta",
-"Damanhur",
-"al-Minya",
-"Beni Suef",
-"Qena",
-"Sohag",
-"Hurghada",
-"th of October City",
-"Shibin El Kom",
-"Banha",
-"Kafr el-Sheikh",
-"Arish",
-"Mallawi",
-"10th of Ramadan City",
-"Bilbais",
-"Marsa Matruh",
-"Idfu",
-"Mit Ghamr",
-"Al-Hamidiyya",
-"Desouk",
-"Qalyub",
-"Abu Kabir",
-"Kafr el-Dawwar",
-"Girga",
-"Akhmim",
-"Matareya");
-$periods = array("Not Specified", "Summer Internship", "Winter Internship");
-$types = array("Full-time", "Part-time");
-$academic_years = array(1,2,3,4,5,6,7);
+$fields = getConstant("fields");
+$cities = getConstant("cities");
+$periods = getConstant("periods");
+$types = getConstant("types");
+$academic_years = getConstant("academic_year");
 function checkExists($key, $array)
 {
 	if (isset($array[$key])) return $array[$key];
@@ -78,7 +22,7 @@ function sanitizeField($value)
 if ($_POST)
 {
 	$errors = array();
-	foreach ($required_fields as $field) { if (!isset($_POST[$field])) { $errors[] = "'".ucfirst(getName($field))	."' is a required field, but it was left blank.";} } 
+	foreach ($required_fields as $field) { if (!isset($_POST[$field]) || empty($_POST[$field])) { $errors[] = "'".ucfirst(getName($field))	."' is a required field, but it was left blank.";} } 
 	
 	if (!$errors)
 	{
