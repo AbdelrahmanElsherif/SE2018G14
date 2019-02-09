34f11f747a75facbb7bc64cee8ec868be0955370
Doha Mustafa Niazy <36457313+DohaMustafa@users.noreply.github.com>
2019-02-10 00:31:40 +0200

    Add files via upload
    Manage_Application

---
+++ backend/application.php
@@ -0,0 +1,44 @@
+<?php
+require_once("common.php");
+requiresAuthentiction();
+
+
+
+function application ($id)
+{
+$applications = mysql_select("application","",array("internship_id=>$id"));
+$applications=$applications->fetch();
+return $applications;
+}
+
+
+
+if($_POST)
+{
+  if(isset($_POST['status']) && isset($_POST['application_id']))
+  {
+    dosql("UPDATE internships SET status=:status WHERE id=:id",
+    array("status"=> $_POST['status'],"id"=>$POST['application_id']));
+  }
+
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
