71d9a333e4ad891c8306a9c1aa0bba33e4de3cf5
SamaaMoaty <36595724+SamaaMoaty@users.noreply.github.com>
2019-02-09 03:23:30 +0200

    Update final_index.php

--- final_index.php
+++ final_index.php
@@ -1,4 +1,8 @@
-<?php require_once('header.php'); ?>
+<?php include ('common.php');
+$css = array("css/style.css");
+require_once("global_header.php");
+?>
+
 <style>
 body {font-family: Arial, Helvetica, sans-serif; background-image: url(slide_one.jpg); font-size: 20px ;background-repeat:no-repeat;background-position: 50% 22% ; background-size: 100%; }
 button {
@@ -14,4 +18,4 @@ button {
 
 <p>Intern.com   <button onclick="index.php" style="width:15%; margin:2px;">Home</button>
 <button onclick="#" style="width:15%; margin:2px;">Explore</button><button onclick="#" style="width:15%; margin:2px;">Post Internship</button><button onclick="post_internship.php" style="width:15%; margin:2px;">Manage Application</button></p>
-<?php require_once("footer.php"); ?>
+<?php require_once("global_footer.php"); ?>
