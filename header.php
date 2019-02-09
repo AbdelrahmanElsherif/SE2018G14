ff424b31831d6c03059221034bdfc6bb0b482552
aliamaim <ali.amaim@yahoo.com>
2019-02-10 00:40:09 +0200

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@ require_once("view_functions.php");
 ?>
 <div class="content">
    <header>
-<nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <a class="navbar-brand" href="index.php">Intern</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
