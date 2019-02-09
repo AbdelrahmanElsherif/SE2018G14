ed8817b683742253f312eb78a604e43162a2d590
SamaaMoaty <36595724+SamaaMoaty@users.noreply.github.com>
2019-02-09 03:18:22 +0200

    Create final_index.php

---
+++ final_index.php
@@ -0,0 +1,17 @@
+<?php require_once('header.php'); ?>
+<style>
+body {font-family: Arial, Helvetica, sans-serif; background-image: url(slide_one.jpg); font-size: 20px ;background-repeat:no-repeat;background-position: 50% 22% ; background-size: 100%; }
+button {
+  font-size: 15px;
+  padding: auto;
+  margin:  auto;
+  border: none;
+  background-color: inherit;
+  cursor: pointer;
+  width: 100%;
+}
+</style>
+
+<p>Intern.com   <button onclick="index.php" style="width:15%; margin:2px;">Home</button>
+<button onclick="#" style="width:15%; margin:2px;">Explore</button><button onclick="#" style="width:15%; margin:2px;">Post Internship</button><button onclick="post_internship.php" style="width:15%; margin:2px;">Manage Application</button></p>
+<?php require_once("footer.php"); ?>
