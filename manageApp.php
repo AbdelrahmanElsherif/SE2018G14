82d375acdae3d949bc891e3906aeba283416ce38
SamaaMoaty <36595724+SamaaMoaty@users.noreply.github.com>
2019-02-10 11:27:44 +0200

    Update manageApp.php

--- manageApp.php
+++ manageApp.php
@@ -5,5 +5,6 @@
  body {font-family: Arial, Helvetica, sans-serif; background-image: url(slide_one.jpg); font-size: 20px ;background-repeat:no-repeat;background-position: 50% 22% ; background-size: 100%; }
  </style>
 </div>
-<h1 text-align='center'>Application for:</h1>
+</br>
+<h1  style="text-align:center;">Application for:</h1>
 <?php require_once("footer.php"); ?>
