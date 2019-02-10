277d03a953e31632eadf3682bcf37083afba9c1f
SamaaMoaty <36595724+SamaaMoaty@users.noreply.github.com>
2019-02-11 01:43:58 +0200

    Delete samaa.php

--- samaa.php
+++
@@ -1,7 +0,0 @@
-<?php include ('common.php');
-$css = array("css/style.css");
-require_once("header.php");
-<div class="content container">
-<h2>Title</h2>
-</div>
-<?php require_once("footer.php"); ?>
