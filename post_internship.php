c7ff63da5718ee33d98f898829f2129fcb8a3eda
SamaaMoaty <36595724+SamaaMoaty@users.noreply.github.com>
2019-02-11 00:52:03 +0200

    Update post_internship.php

--- post_internship.php
+++ post_internship.php
@@ -1,6 +1,10 @@
 <?php require_once('header.php'); 
 ?>
-<div class="content container">
+<style>
+
+ body {font-family: Arial, Helvetica, sans-serif; background-image: url(slide_one.jpg); font-size: 30px ;background-repeat:no-repeat;background-position: 50% 22% ;  background-size: 100%; }
+</style>
+  <div class="content container">
 <h2>Post Internship</h2>
 <?php require_once("errors.php"); ?>
 <form method='POST'>
