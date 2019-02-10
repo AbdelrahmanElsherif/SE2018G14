b0bad0a1212c5bffb71a04d0a64e836e6aa33644
SamaaMoaty <36595724+SamaaMoaty@users.noreply.github.com>
2019-02-10 22:39:22 +0200

    Update Apply.php

--- Apply.php
+++ Apply.php
@@ -4,11 +4,8 @@
 <style>
  body {font-family: Arial, Helvetica, sans-serif; background-image: url(slide_one.jpg); font-size: 20px ;background-repeat:no-repeat;background-position: 50% 22% ; background-size: 100%; }
  </style>
+ </br>
  <h1  style="text-align:center;">Application for:</h1>
 </div>
-</br>
-
-
-
 <?php require_once("footer.php"); ?>
 
