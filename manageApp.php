5fa33acb74e6daf5c5283827bb162436322682ca
SamaaMoaty <36595724+SamaaMoaty@users.noreply.github.com>
2019-02-10 11:08:24 +0200

    Create manageApp.php

---
+++ manageApp.php
@@ -0,0 +1,8 @@
+
+<?php require_once('header.php'); ?>
+<div class="content container">
+<style>
+ body {font-family: Arial, Helvetica, sans-serif; background-image: url(slide_one.jpg); font-size: 20px ;background-repeat:no-repeat;background-position: 50% 22% ; background-size: 100%; }
+ </style>
+</div>
+<?php require_once("footer.php"); ?>
