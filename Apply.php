02a8ba219591b5087ee7ed5d9c54a441905028e0
SamaaMoaty <36595724+SamaaMoaty@users.noreply.github.com>
2019-02-10 22:35:19 +0200

    Update Apply.php

--- Apply.php
+++ Apply.php
@@ -5,6 +5,7 @@
  body {font-family: Arial, Helvetica, sans-serif; background-image: url(slide_one.jpg); font-size: 20px ;background-repeat:no-repeat;background-position: 50% 22% ; background-size: 100%; }
  </style>
 </div>
+</br>
 <h1  style="text-align:center;">Application for:</h1>
 
 
