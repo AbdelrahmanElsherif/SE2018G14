e805faf3f92d7f89dc71f2487da80c612800bcba
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-12 23:20:06 +0200

    Update search.php

--- backend/search.php
+++ backend/search.php
@@ -19,12 +19,12 @@ if (isset($_GET['q']))
 			$params['company'] = htmlentities(strip_tags("%".$_GET['q']."%"));
 			$params['description'] = htmlentities(strip_tags("%".$_GET['q']."%"));
 		}
-		if (isset($_GET['field'])) $params['field'] = checkExists($_GET['field'], $fields);
+		if (isset($_GET['field']) && checkExists($_GET['field'], $fields)) $params['field'] = checkExists($_GET['field'], $fields);
 		
-		if (isset($_GET['city'])) $params['city'] = checkExists($_GET['city'], $cities);
-		if (isset($_GET['period'])) $params['period']= checkExists($_GET['period'], $periods);
-		if (isset($_GET['type'])) $params['type'] = checkExists($_GET['type'], $types);
-		if (isset($_GET['academic_year'])) $params['academic_year'] = checkExists($_GET['academic_year'], $academic_years);
+		if (isset($_GET['city']) && checkExists($_GET['city'], $cities)) $params['city'] = checkExists($_GET['city'], $cities);
+		if (isset($_GET['period']) && checkExists($_GET['period'], $periods)) $params['period']= checkExists($_GET['period'], $periods);
+		if (isset($_GET['type']) && checkExists($_GET['type'], $types)) $params['type'] = checkExists($_GET['type'], $types);
+		if (isset($_GET['academic_year']) && checkExists($_GET['academic_year'], $academic_years)) $params['academic_year'] = checkExists($_GET['academic_year'], $academic_years);
 		
 		$stmt = mysql_select("internship", "AND" , $params, " LIKE ");
 	}
