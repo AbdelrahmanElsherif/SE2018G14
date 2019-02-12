e1b04141f0de3e183bbe0f2b36a6b57aabf50de5
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-13 01:55:25 +0200

    Update download.php

--- backend/download.php
+++ backend/download.php
@@ -5,6 +5,7 @@ if (isset($_GET['id']) && $_GET['id'])
 {
 	$application = getApplication($_GET['id']);
 	print_r($application);
+	exit();
 	if ($application)
 	{
 		if (hasAccess($application['internship_id'], $_SESSION['user']['id']) || $application['user_id'] == $_SESSION['user']['id'])
