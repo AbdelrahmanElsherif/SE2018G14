0199c4748e1c00563eb80dc04324fc379c6d646f
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-13 01:53:39 +0200

    Debugging

--- backend/download.php
+++ backend/download.php
@@ -4,6 +4,7 @@ require_once("functions.php");
 if (isset($_GET['id']) && $_GET['id'])
 {
 	$application = getApplication($_GET['id']);
+	print_r($application);
 	if ($application)
 	{
 		if (hasAccess($application['internship_id'], $_SESSION['user']['id']) || $application['user_id'] == $_SESSION['user']['id'])
