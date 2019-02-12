14bd4beb65399442ebb9d87233e9b76f29a69ff4
SamaaMoaty <36595724+SamaaMoaty@users.noreply.github.com>
2019-02-13 00:47:04 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,22 @@
-</div>
-</div>
-<?php require_once("global_footer.php"); ?>
+<?php require_once('common.php');
+requiresAuthentication();
+$css = array("css/main.css");
+require_once("global_footer.php");
+require_once("view_fu
+nctions.php");
+?>
+<style>
+.footer {
+ position: fixed;
+ left: 0;
+ bottom: 0;
+ width: 100%;
+ background-color: rgb(108,117,125);
+ color: white;
+ text-align: center;
+}
+</style>
 
+<div class="footer">
+ <p style="font-size:16px; color:'balck';">SE2018G14 @ 2019 &nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;&nbsp; About &nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;&nbsp; Terms of use &nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;&nbsp; Privacy Policy &nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;&nbsp; Developers  &nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;&nbsp; Language  &nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;&nbsp; mobile  &nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;&nbsp;&nbsp; Intern.com  </p>
+</div>
