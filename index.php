d00a3fdf20fdc4adaf49bc0083f98a9b4eb1b02a
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-13 00:49:23 +0200

    Add both cards on the same row

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
  body {font-family: Arial, Helvetica, sans-serif; background-image: url(slide_one.jpg); font-size: 20px ;background-repeat:no-repeat;background-position: 50% 22% ; background-size: 100%; }
  </style>-->
 <h1>Featured</h1>
-<div class="col-md-6">
-<div class="card" style="width: 18rem;">
+<div class="row">
+<div class="col-md-3">
+<div class="card">
   <div class="card-body">
     <h5 class="card-title">Explore</h5>
     <p class="card-text">Search through <?php echo $internships_count; ?> available internships.</p>
@@ -13,13 +14,14 @@
   </div>
 </div>
 </div>
-<div class="col-md-6">
-<div class="card" style="width: 18rem;">
+<div class="col-md-3">
+<div class="card">
   <div class="card-body">
     <h5 class="card-title">Post Internship</h5>
     <p class="card-text">Post an internship opportunity for interns to apply online.</p>
     <a href="post_internship.php" class="btn btn-primary">Go There</a>
   </div>
+</div>
 </div>
 </div>
  <?php require_once("footer.php"); ?>
