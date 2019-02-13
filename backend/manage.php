edacccd2e7f324a17a77dbc9ad932c23a6997f91
Giovanni Mounir <GiovanniMounir@users.noreply.github.com>
2019-02-13 03:21:20 +0200

    Update manage.php

--- backend/manage.php
+++ backend/manage.php
@@ -1,5 +1,5 @@
 <?php
 require_once("functions.php");
 $internship_stmt = mysql_select("internship", "", array("user_id" => $_SESSION['user']['id']));
-$application_stmt = mysql_select("application", "", array("user_id" => $_SESSION['user']['id']), "internship_id");
+$application_stmt = dosql("SELECT internship_id FROM application INNER JOIN internship ON application.user_id=:user_id AND internship.id=application.internship_id", array(":user_id" => $_SESSION['user']['id']));
 ?>
