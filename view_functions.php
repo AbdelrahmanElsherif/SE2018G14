6ef5b637ed39d8409be50481e6662b7881a45f84
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-13 07:51:31 +0200

    Update view_functions.php

--- view_functions.php
+++ view_functions.php
@@ -19,13 +19,12 @@ function showSearch($stmt, $link = "apply", $text = "")
 	  while ($row = $stmt -> fetch()){
 		  $id = $row['id'];
 		  
-		  if ($link == "apply") { if (getApplication($row['id'], $_SESSION['user']['id'], "id")) $link = false; } //This will be SQL-intensive; we will use pagination. There might be better practice.
-		  unset($row['id']);
+		 unset($row['id']);
 		  unset($row['user_id']);
 		  unset($row['internship_id']);
 		  unset($row['cv']);
 		  $row['description'] = nl2br($row['description']);
-        generateRow($row, $link? array("<a href='".$link.".php?internship_id=".$id."'>".ucfirst($text)."</a>") : array("-"));
+        generateRow($row, ($link && getApplication($id, $_SESSION['user']['id'], "id") || hasAccess($id, $_SESSION['user']['id']))? array("<a href='".$link.".php?internship_id=".$id."'>".ucfirst($text)."</a>") : array("-"));
        }
 	 echo '</tbody></table>';
 }
