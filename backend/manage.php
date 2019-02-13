da533925b49ddbe40fb43af1c8291738a309b4c4
Giovanni Mounir <GiovanniMounir@users.noreply.github.com>
2019-02-13 03:18:43 +0200

    Update manage.php

--- backend/manage.php
+++ backend/manage.php
@@ -1,5 +1,5 @@
 <?php
 require_once("functions.php");
 $internship_stmt = mysql_select("internship", "", array("user_id" => $_SESSION['user']['id']));
-$application_stmt = mysql_select("application", "", array("user_id" => $_SESSION['user']['id']));
+$application_stmt = mysql_select("application", "", array("user_id" => $_SESSION['user']['id']), "internship_id");
 ?>
