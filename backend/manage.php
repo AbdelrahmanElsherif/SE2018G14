aaf962fec7e38c27ad1b43d4810ce4939b7b9ee7
Giovanni Mounir <GiovanniMounir@users.noreply.github.com>
2019-02-13 03:22:59 +0200

    Update manage.php

--- backend/manage.php
+++ backend/manage.php
@@ -1,5 +1,5 @@
 <?php
 require_once("functions.php");
 $internship_stmt = mysql_select("internship", "", array("user_id" => $_SESSION['user']['id']));
-$application_stmt = dosql("SELECT internship_id FROM application INNER JOIN internship ON application.user_id=:user_id AND internship.id=application.internship_id", array(":user_id" => $_SESSION['user']['id']));
+$application_stmt = dosql("SELECT * FROM internship INNER JOIN application ON application.user_id=:user_id AND internship.id=application.internship_id", array(":user_id" => $_SESSION['user']['id'])); //bad performance, we will figure something out later
 ?>
