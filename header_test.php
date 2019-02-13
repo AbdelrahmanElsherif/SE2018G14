e0030dd27968fff1f3675f6497ef0fda871e43db
aliamaim <ali.amaim@yahoo.com>
2019-02-13 19:13:26 +0200

    Update header_test.php

--- header_test.php
+++ header_test.php
@@ -5,14 +5,15 @@ require_once("global_header.php");
 require_once("view_functions.php");
 ?>
 
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="js/header_test.js"></script>
 
-
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="css/header_test.css">
-<script src="js/header_test.js"></script>
+
 
 <header id="header">
     <div class="container">
