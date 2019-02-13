905b9ce86f7155b3a9c53c84e24b9f511c7c43ba
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-13 07:07:52 +0200

    Update manage.php

--- manage.php
+++ manage.php
@@ -8,7 +8,33 @@ require_once("header.php");?>
  
 <h1>List of your applications:</h1>
  <?php
- showSearch($application_stmt, "application", "Manage");
+ 	echo '
+	    <table class="table">
+	<thead>
+      <th>Position</th>
+      <th>Company</th>
+      <th>Field</th>
+      <th>City</th>
+      <th>Period</th>
+      <th>Type</th>
+      <th>Academic Year</th>
+      <th>Description</th>
+      <th>Mobile Number</th>
+      <th>Status</th>
+      <th></th>
+	  </thead>
+	  <tbody>';
+	  while ($row = $application_stmt -> fetch()){
+		  $id = $row['id'];
+		  unset($row['id']);
+		  unset($row['user_id']);
+		  unset($row['internship_id']);
+		  unset($row['cv']);
+		  print_r($row);
+		  $row['description'] = nl2br($row['description']);
+		  generateRow($row, ($row['status'] == -1)? array("<a href='#'>Retract</a>" : ''));
+       }
+	 echo '</tbody></table>';
  
  ?>
 <?php require_once("footer.php"); ?>
