7e44fd4c308bbd3fa05ed4386528e89873b116df
aliamaim <ali.amaim@yahoo.com>
2019-02-13 18:42:00 +0200

    Update header_test.php

--- header_test.php
+++ header_test.php
@@ -8,7 +8,7 @@ require_once("view_functions.php");
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="css/header_test.css">
