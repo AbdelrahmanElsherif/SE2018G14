70fe8607c6ced9b20e700a453778ed38ab987cad
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-14 04:24:54 +0200

    Update apply.php

--- apply.php
+++ apply.php
@@ -4,27 +4,32 @@ $pre_title = "Apply";
 require_once("header.php");
 ?>
 <div class="content container">
- <form method="POST" enctype="multipart/form-data">
  <?php include ('errors.php'); ?>
- <div class="col-md-6">
+ <div class="col-md-12">
+  <div class="col-md-4 offset-md-3">
    <p>Company: <b><?php echo $internship['company']; ?></b></p>
    <p>Position: <b><?php echo $internship['role']; ?></b></p>
    <p>Academic Year: <b><?php echo $internship['academic_year']; ?></b></p>
+ </div>
+ <div class="col-md-3">
    <p>City: <b><?php echo $internship['city']; ?></b></p>
    <p>Type: <b><?php echo $internship['type']; ?></b></p>
    <p>Period: <b><?php echo $internship['period']; ?></b></p>
   </div>
+  <div class="col-md-12">
  <hr class="mt-4 mb-5"> 
- <div class="col-md-6">
+ <form method="POST" enctype="multipart/form-data">
    <?php
    generateInput("Mobile");
    generateInput("CV File", "file");
    ?>
    <hr class="mt-4 mb-5">
    <input type="hidden" name="internship_id" value="<?php echo $internship['id']; ?>"></input>
-   </div>
+   <div class="text-center">
    <input type="submit" class="btn btn-primary" value="Apply"></input>
- </form>
+   </div>
+    </form>
+   </div>
 
 </div>
 <?php require_once("footer.php"); ?>
