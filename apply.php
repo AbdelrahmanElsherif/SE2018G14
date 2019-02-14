65c422ad58c6f095e007a06847780ecb2b11583a
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-14 04:17:46 +0200

    Update apply.php

--- apply.php
+++ apply.php
@@ -1,62 +1,23 @@
 <?php
 $page_title = "Management";
-$pre_title = "Apply for: " . $internship['role'];
+$pre_title = "Apply";
 require_once("header.php");
 ?>
-
-
-<!--
-<style>
- body { background-image: url(slide_one.jpg) ;background-repeat:no-repeat;background-position: 50% 22% ;  background-size: 100%; }
-
- .container {
-   padding: 20px;
- }
- input[type=text], input[type=file] {
-   width: 100%;
-   padding: 15px;
-   margin: 5px 0 22px 0;
-   display: inline-block;
-   border: none;
-   background: #f1f1f1;
- }
-
- input[type=text]:focus, input[type=file]:focus {
-   background-color: #ddd;
-   outline: none;
- }
- button {
-   background-color:rgb(108,117,125);
-   font-size: 20px;
-   color: black;
-   padding: 10px 10px;
-   margin: 4px 0;
-   border: none;
-   cursor: pointer;
-   width: 100%;
- }
- .submitbtn {
-   background-color: rgb(108,117,125);
-   color: white;
-   padding: 16px 20px;
-   margin: 8px 0;
-   border: none;
-   cursor: pointer;
-   width: 50%;
-
-   right: 100%;
-   margin-left: 50%;
- }
-  </style>
-  -->
- </br>
-</br>
 <div class="content container">
  <form method="POST" enctype="multipart/form-data">
-   <?php include ('errors.php');
+ <?php include ('errors.php'); ?>
+   <p>Company: <b><?php echo $internship['company']; ?></b></p>
+   <p>Position: <b><?php echo $internship['role']; ?></b></p>
+   <p>Academic Year: <b><?php echo $internship['academic_year']; ?></b></p>
+   <p>City: <b><?php echo $internship['city']; ?></b></p>
+   <p>Type: <b><?php echo $internship['type']; ?></b></p>
+   <p>Period: <b><?php echo $internship['period']; ?></b></p>
+ <hr class="mt-4 mb-5">
+   <?php
    generateInput("Mobile");
    generateInput("CV File", "file");
    ?>
+   <hr class="mt-4 mb-5">
    <input type="hidden" name="internship_id" value="<?php echo $internship['id']; ?>"></input>
    <input type="submit" class="btn btn-primary" value="Apply"></input>
  </form>
