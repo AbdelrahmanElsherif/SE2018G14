21c284c0d8987e877aa0e28af301edc219526626
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-14 04:54:01 +0200

    Update application.php

--- application.php
+++ application.php
@@ -4,7 +4,24 @@ $pre_title = "Manage Internship";
 require_once("header.php");
 require_once("backend/functions.php");
 ?>
-  <table class="table">
+<div class="row">
+          <div class="col-12 col-xl-12">
+            
+            <!-- Goals -->
+            <div class="card">
+              <div class="card-header">
+                <div class="row align-items-center">
+                  <div class="col">
+                    <!-- Title -->
+                    <h4 class="card-header-title">
+                      Applications
+                    </h4>
+                  </div>
+                </div> <!-- / .row -->
+              </div>
+              <div class="table-responsive mb-0" data-toggle="lists" data-lists-values="[&quot;goal-project&quot;, &quot;goal-status&quot;, &quot;goal-progress&quot;, &quot;goal-date&quot;]">
+                
+<table class="table table-sm card-table">
     <thead>
       <th>ID</th>
 	  <th>Name</th>
@@ -18,7 +35,7 @@ require_once("backend/functions.php");
     while ($row = $stmt->fetch()) {
 		$row['user_id'] = getUser($row['user_id'])['name'];
 		$row[] = "<a href='download.php?id=".$row['id']."'>Download CV</a>";
-		if ($row['status'] == -1) { $row[] = "<form method='POST' action='application.php?internship_id=".intval($_GET['internship_id'])."'><input type='submit' name='accept_".$row['id']."' class='btn btn-success' value='Accept' /> <input type='submit' name='reject_".$row['id']."' class='btn btn-danger' value='Reject' /></form>"; } else { $row[] = processStatus($row['status']); } 
+		if ($row['status'] == -1) { $row[] = "<form method='POST' action='application.php?internship_id=".intval($_GET['internship_id'])."'><input type='submit' name='accept_".$row['id']."' class='btn btn-success btn-sm' value='Accept' /> <input type='submit' name='reject_".$row['id']."' class='btn btn-danger btn-sm' value='Reject' /></form>"; } else { $row[] = processStatus($row['status']); } 
 		unset($row['status']);
 		generateRow($row);
     }
