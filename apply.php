3fb87a853f8fca460cd88ec4765bab0d4171a65e
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-14 04:18:52 +0200

    Update apply.php

--- apply.php
+++ apply.php
@@ -6,19 +6,23 @@ require_once("header.php");
 <div class="content container">
  <form method="POST" enctype="multipart/form-data">
  <?php include ('errors.php'); ?>
+ <div class="col-md-6">
    <p>Company: <b><?php echo $internship['company']; ?></b></p>
    <p>Position: <b><?php echo $internship['role']; ?></b></p>
    <p>Academic Year: <b><?php echo $internship['academic_year']; ?></b></p>
    <p>City: <b><?php echo $internship['city']; ?></b></p>
    <p>Type: <b><?php echo $internship['type']; ?></b></p>
    <p>Period: <b><?php echo $internship['period']; ?></b></p>
- <hr class="mt-4 mb-5">
+  </div>
+ <hr class="mt-4 mb-5"> 
+ <div class="col-md-6">
    <?php
    generateInput("Mobile");
    generateInput("CV File", "file");
    ?>
    <hr class="mt-4 mb-5">
    <input type="hidden" name="internship_id" value="<?php echo $internship['id']; ?>"></input>
+   </div>
    <input type="submit" class="btn btn-primary" value="Apply"></input>
  </form>
 
