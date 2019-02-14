57ba4cfdc6b6c11c2289a3f9bdc55d6136837c17
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-14 13:39:51 +0200

    Update global_header.php

--- global_header.php
+++ global_header.php
@@ -2,6 +2,7 @@
 <html lang="en" dir="ltr">
 <head>
 <title>Intern.com</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link href="css/bootstrap.css" rel="stylesheet"><?php if (isset($css)) foreach ($css as $link) echo "<link href='".$link."' rel='stylesheet'>"; ?>
 <script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.js"></script>
