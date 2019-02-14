b2d4f19d7b368df1942c568a04a6311b9d245531
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-14 06:04:47 +0200

    Update notifications.php

--- backend/notifications.php
+++ backend/notifications.php
@@ -1,5 +1,5 @@
 <?php
 require_once("functions.php");
-if (isset($_GET['m']) && $_GET['m'] == "api") { json_encode(getNotifications($_SESSION['user']['id'])->fetchAll()); exit; }
+if (isset($_GET['m']) && $_GET['m'] == "api") { echo json_encode(getNotifications($_SESSION['user']['id'])->fetchAll()); exit; }
 $stmt = getNotifications($_SESSION['user']['id']);
 ?>
