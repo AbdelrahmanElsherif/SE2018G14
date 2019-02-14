908378c31b18415cfa405a4b210febba793d7aea
Giovanni Mounir <giovannimounir@gmail.com>
2019-02-14 06:25:07 +0200

    Update notifications.php

--- backend/notifications.php
+++ backend/notifications.php
@@ -1,6 +1,7 @@
 <?php
 require_once("functions.php");
-if (isset($_GET['m']) && $_GET['m'] == "api") { if (isset($_GET['a']) && $_GET['a'] == "unread") { echo hasUnread($_SESSION['user']['id']); } else { echo json_encode(getNotifications($_SESSION['user']['id'])->fetchAll()); } exit; }
-$stmt = getNotifications($_SESSION['user']['id']);
+if (isset($_GET['m']) && $_GET['m'] == "api") { if (isset($_GET['a']) && $_GET['a'] == "unread") { echo hasUnread($_SESSION['user']['id']); } else { echo json_encode(getNotifications($_SESSION['user']['id'])->fetchAll()); } }
 dosql("UPDATE notifications SET seen=1 WHERE user_id=:user_id", array(":user_id" => $_SESSION['user']['id']));
+if (isset($_GET['m']) && $_GET['m'] == "api") exit;
+$stmt = getNotifications($_SESSION['user']['id']);
 ?>
